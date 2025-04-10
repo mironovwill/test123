0398ff925462c635987fc24a9b046e45d1d4a399
d.mironov <d.mironov@tandp.ru>
2025-04-10 12:26:39 +0300

    Обновлены данные в файле test-data.json, включая изменения в структурах и пользователях. Добавлены новые тестовые данные для e-course. Оптимизированы импорты в тестах для повышения читаемости и удобства использования.

---
+++ src/tests/admin/topics-create/create-e-course-topic.spec.ts
@@ -0,0 +1,27 @@
+import { appFixture as test } from '@core/fixtures';
+import { eCourseTopicInfo } from './test-data/e-course.test-data';
+
+const { authors, company, place } = eCourseTopicInfo;
+let topicId: string;
+
+test.describe(
+  `Создание и проверка топика с типом "Электронный курс"`,
+  { tag: ['@Regress', '@Admin', '@Электронный курс'] },
+  () => {
+    test.beforeEach(async ({ createTopicByUI }) => {
+      topicId = await createTopicByUI(eCourseTopicInfo);
+    });
+
+    test(`Проверка топика с типом "Электронный курс" @allure.id=191"`, async ({
+      adminTopicPage,
+    }) => {
+      await adminTopicPage.visitTopic(topicId);
+      await adminTopicPage.validateBasicTopicInfo(eCourseTopicInfo, topicId);
+      await adminTopicPage.validateAdditionalTopicInfo(eCourseTopicInfo);
+      await adminTopicPage.validateTopicAuthors(authors!);
+      //TODO: Стиль, время начала, длительность
+      await adminTopicPage.validateTopicCompany(company!);
+      await adminTopicPage.validateMeetPlace(place!);
+    });
+  },
+);
--- src/tests/admin/topics-create/test-data/index.ts
+++ src/tests/admin/topics-create/test-data/index.ts
@@ -3,3 +3,4 @@ export * from './full-time-corporate-course.test-data';
 export * from './full-time-external-course.test-data';
 export * from './online-corporate-course.test-data';
 export * from './online-external-course.test-data';
+export * from './e-course.test-data';
